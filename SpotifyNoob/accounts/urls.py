48d03e7113a91bf624f323770ced95a445970f26
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-10-08 00:42:55 +0100

    Delete old project

--- SpotifyNoob/accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-# Urls patterns
-urlpatterns = [
-    path("/", views.index, name="home"),
-]
--- SpotifyNoob/accounts/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import request
-
-# Create your views here.
-def index(request):
-    return render(request, "accounts/templates/index.html")
